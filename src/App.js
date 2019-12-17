ad9f2d218c7c83b6911025857a7cf63954da1ac8
Ryan Ghaida <ryaboug@gmail.com>
2019-12-17 17:28:51 -0500

    css grid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import background from './background.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+var styles = {
+  backgroundImage: `url(${background})`,
+  backgroundSize: '80% 140%',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center'
+
+};
+
+class App extends React.Component {
+  render() {
+    return (
+    <div id="main" style={styles}> 
+    <div className="grid-container">
+    <aside className="grid-sidebar">1</aside>
+    <div className="grid-item">2</div>
+    <div className="grid-item">3</div>
+    <div className="grid-item">4</div>
+    <div className="grid-item">5</div>
+    <div className="grid-item">6</div>
+    <div className="grid-item">7</div>
+    <div className="grid-item">8</div>
+    <aside className="grid-sidebar">9</aside>
+    <div className="grid-item">10</div>
+    <div className="grid-item">11</div>
+    <div className="grid-item">12</div>
+    <div className="grid-item">13</div>
+    <div className="grid-item">14</div>
     </div>
-  );
+    </div>)
+  }
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div id="Navbar">
+        <h1> Navbar </h1> 
+      </div> 
+    )
+
+
+  }
+}
+
+export default Navbar;
